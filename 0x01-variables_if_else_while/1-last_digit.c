065f332fe691bd4d8242bf97d0db95bb49f72b3c
Boldfive <abisoyeomolade11@gmail.com>
2022-03-14 15:38:21 -0700

    My 1 task commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -24,7 +24,7 @@ printf("Last digit of %i is %i and is 0\n", n, n % 10);
 
 else
 {
-printf("last digit of %i is % and is less than 6 and not 0\n", n, n % 10);
+printf("last digit of %i is %i and is less than 6 and not 0\n", n, n % 10);
 }
 
 return(0);
