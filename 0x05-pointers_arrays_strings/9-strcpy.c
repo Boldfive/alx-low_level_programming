16384de679443279654abf2c08034daf5cca977b
Boldfive <abisoyeomolade11@gmail.com>
2022-03-22 17:44:12 -0700

    My 9th task commit

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *_strcpy - copy the string pointed to by src
+ *@dest: Apointer to a char that will be changed
+ *@src: A pointer to a char that will be changed
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+
+int i;
+
+i = 0;
+
+while (src[i] != '\0')
+{
+dest[i] = src[i];
+i++;
+}
+dest[i] = '\0';
+
+return (dest);
+}
