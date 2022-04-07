e39615d6f561b3695d5bee2392c8d9cc6bbc924e
Boldfive <abisoyeomolade11@gmail.com>
2022-04-07 13:40:42 -0700

    My 1-string_nconcat.c commit

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,69 +1,42 @@
 #include "main.h"
-/**
- *_strlen - count array
- *@s: array of elements
- *Return: int i
- */
-int _strlen(char *s)
-{
-unsigned int i;
-
-i = 0;
-while (s[i] != '\0') /*Count charater of string*/
-{
-i++;
-}
-return (i);
-}
+#include <stdlib.h>
 
 /**
- *string_nconcat - concatenates two strings
- *@s1: string
- *@s2: string
- *@n: number bytes
- *Return: char dst
+ * string_nconcat - concatenates two strings.
+ * @s1: first string to copy
+ * @s2: second string to copy
+ * @n: number of bytes of s2 to copy
+ *
+ * Return: char pointer to newly allocated place in memory
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-char *dst;
-unsigned int i, count, j, size, size_1, size_2;
+	unsigned int i, j, k;
+	char *s;
 
-if (s1 == NULL) /*If the array is empty*/
-s1 = "";
-
-if (s2 == NULL)
-s2 = "";
-
-size_1 = _strlen(s1);
-size_2 = _strlen(s2);
-
-if (n >= size_2)
-{
-count = size_2;
-}
-else
-{
-count = n;
-}
-/*count size total*/
-size = size_1 + count + 1;
-/*malloc*/
-dst = (char *) malloc(size * sizeof(char));
-if (dst == 0)
-{
-return (NULL);
-}
-/*Concantenate arrays*/
-{
-for (i = 0; *(s1 + i) != '\0'; i++)
-*(dst + i) = *(s1 + i);
-for (j = 0; j < count; j++)
-{
-*(dst + i) = *(s2 + j);
-i++;
-}
-*(dst + i) = '\0';
-}
-return (dst);
-
+	if (s1 == NULL)
+		i = 0;
+	else
+	{
+		for (i = 0; s1[i]; i++)
+			;
+	}
+	if (s2 == NULL)
+		j = 0;
+	else
+	{
+		for (j = 0; s2[j]; j++)
+			;
+	}
+	if (j > n)
+		j = n;
+	s = malloc(sizeof(char) * (i + j + 1));
+	if (s == NULL)
+		return (NULL);
+	for (k = 0; k < i; k++)
+		s[k] = s1[k];
+	for (k = 0; k < j; k++)
+		s[k + i] = s2[k];
+	s[i + j] = '\0';
+	return (s);
 }
